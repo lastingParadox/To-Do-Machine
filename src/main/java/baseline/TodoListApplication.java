b4d69cee8e909db856117b19979089d389ce99ae
lastingParadox <zanpres@gmail.com>
2021-11-03 00:43:30 -0400

    Added some resources, continued CSS modification

--- src/main/java/baseline/TodoListApplication.java
+++ src/main/java/baseline/TodoListApplication.java
@@ -8,6 +8,7 @@ package baseline;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.util.Objects;
@@ -19,6 +20,7 @@ public class TodoListApplication extends javafx.application.Application {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("main.fxml")));
 
         Scene scene = new Scene(root);
+        stage.setResizable(false);
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("styles.css")).toExternalForm());
         stage.setTitle("To-Do Machine");
         stage.setScene(scene);
@@ -26,6 +28,7 @@ public class TodoListApplication extends javafx.application.Application {
     }
 
     public static void main(String[] args) {
+        Font.loadFont(TodoListApplication.class.getResourceAsStream("Orbitron_Regular.ttf"), 16);
         launch(args);
     }
 }
